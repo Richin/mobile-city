880aeeb82523292dc33d63e682714ca345014bd4
Richin Varghese <richin1990@gmail.com>
2019-08-28 06:02:11 +0530

    mobile city updatd

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={
+	'ApiUrl':"http://mobilecity-shopping-delivery.akstech.com.sg/driver/api",		
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"mobilecity",
+	'debug': false
+};
